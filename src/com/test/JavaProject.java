958a5db09d4e7f29f351a5bb9bbc67a511a0817c
Ankur Malviya <madanankurmalviya@gmail.com>
2023-12-14 18:14:04 -0800

    my java commit

--- src/com/test/JavaProject.java
+++ src/com/test/JavaProject.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class JavaProject {
 public static void main(String[] args) {
-	System.out.println("My Java Project....");
+	System.out.println("My Java Project....123");
 }
 }
